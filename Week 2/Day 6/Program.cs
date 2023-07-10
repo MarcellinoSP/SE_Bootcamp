df79402bf40c130b6c636b4934eb1b8caeb0931e
MarcellinoSP <marcellinosp64@gmail.com>
2023-07-10 12:00:10 +0700

    Changes

--- Week 2/Day 6/Program.cs
+++ Week 2/Day 6/Program.cs
@@ -10,20 +10,20 @@ public class Program
 		indexNumber = Convert.ToInt32(Console.ReadLine());
 		
 		// USING DELEGATE METHOD, CURRENTLY STILL ERROR
-		BookIdentify books = new BookIdentify();
-		BookDescription getDescription = new BookDescription();
+		// BookIdentify books = new BookIdentify();
+		// BookDescription getDescription = new BookDescription();
 		
-		Book identify; 
-		identify += books.GetBook(in indexNumber);
-		identify += getDescription.DescriptionBook(in indexNumber);
+		// Book identify; 
+		// identify += books.GetBook(in indexNumber);
+		// identify += getDescription.DescriptionBook(in indexNumber);
 		// END OF DELEGATE METHOD
 		
 		// USING SIMPLE METHOD
-		// BookIdentify books = new BookIdentify();
-		// books.GetBook(in indexNumber);
+		BookIdentify books = new BookIdentify();
+		books.GetBook(in indexNumber);
 		
-		// BookDescription getDescription = new BookDescription();
-		// getDescription.DescriptionBook(in indexNumber);
+		BookDescription getDescription = new BookDescription();
+		getDescription.DescriptionBook(in indexNumber);
 		// END OF METHOD
 		Console.ReadLine();
 	}
