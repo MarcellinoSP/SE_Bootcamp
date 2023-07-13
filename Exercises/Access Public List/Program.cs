43272d6851bba32f1fb54284a9e16864adc8dc1f
MarcellinoSP <marcellinosp64@gmail.com>
2023-07-14 04:26:54 +0700

    Add Exercises

---
+++ Exercises/Access Public List/Program.cs
@@ -0,0 +1,32 @@
+﻿using List;
+
+class Program
+{
+	static void Main()
+	{
+		ListTraining listTraining = new();
+		
+		CarModel toyotaCar = new CarModel
+		{
+			brand = "Toyota",
+			series = "Celica",
+			color = "Red",
+			price = 2000
+		};
+		CarModel hondaCar = new CarModel
+		{
+			brand = "Honda",
+			series = "Civic",
+			price = 2000
+		};
+		
+		listTraining.AddList(toyotaCar);
+		listTraining.AddList(hondaCar);
+		
+		string carBrand1 = listTraining.MyCar[0].brand;
+        string carPrice2 = listTraining.MyCar[1].price.ToString();
+		
+		Console.WriteLine(carBrand1);
+        Console.WriteLine(carPrice2);
+	}
+}
--- Exercises/Modulo/Program.cs
+++ Exercises/Modulo/Program.cs
@@ -4,11 +4,20 @@ class Program
 {
 	static void Main()
 	{
-		int a = 3;
-		int b = 5;
-		int n = 15;
+		int a;
+		int b;
+		int n;
 		string stringA = "Foo ";
 		string stringB = "Bar ";
+
+		Console.WriteLine("Insert maximum number: ");
+		n = Convert.ToInt32(Console.ReadLine());
+
+		Console.WriteLine($"Insert 'Foo' Position (1-{n}): ");
+		a = Convert.ToInt32(Console.ReadLine());
+
+		Console.WriteLine($"Insert 'Bar' Position (1-{n})");
+		b = Convert.ToInt32(Console.ReadLine());
 		
 		FooBar generatingFooBar = new FooBar(stringA, stringB, a, b, n);
 		generatingFooBar.getFooBar();
