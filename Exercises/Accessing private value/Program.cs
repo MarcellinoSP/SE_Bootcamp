a7c13feb95c565870652a74f28ccfa1d2d091f9b
MarcellinoSP <marcellinosp64@gmail.com>
2023-07-26 12:13:16 +0700

    Add New Exercises

---
+++ Exercises/Accessing private value/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+	static void Main()
+	{
+		Tile tile = new();
+		tile.GetPosition();
+	}
+}
+
+public class Tile
+{
+	TilePosition position = new();
+	public void GetPosition()
+	{
+		bool message = position.SetPosition(10, 15);
+		Console.WriteLine(message.ToString());
+	}
+
+}
+
+public class TilePosition
+{
+	private int _positionX;
+	private int _positionY;
+	
+	public bool SetPosition(int sizeX, int sizeY)
+	{
+		_positionX = sizeX;
+		_positionY = sizeY;
+		return true;
+	}
+}
