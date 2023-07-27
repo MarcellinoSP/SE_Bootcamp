ead30ac2a4a0d58f7ba609e753b7039b8b85796f
MarcellinoSP <marcellinosp64@gmail.com>
2023-07-27 13:52:22 +0700

    Add Day 17

---
+++ Week 4/Day 17/Dispose/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.IO;
+class Program
+{
+	static void Main()
+	{
+		using (Writing writingNew = new())
+		{
+			writingNew.StartWriting();
+			writingNew.WriteAnother();
+		}
+	}
+}
+
+public class Writing : IDisposable
+{
+	StreamWriter streamWriter;
+	public Writing()
+	{
+		streamWriter = new StreamWriter("output.md");
+		Console.WriteLine("Output created");
+	}
+	
+	public void StartWriting()
+	{
+		streamWriter.WriteLine("Hello!");
+	}
+	
+	public void WriteAnother()
+	{
+		streamWriter.Write("Newbie here!");
+	}
+	
+	public void Dispose()
+	{
+		Dispose(true);
+		GC.SuppressFinalize(this);
+	}
+	
+	protected virtual void Dispose(bool disposing)
+	{
+		if (disposing)
+		{
+			if (streamWriter != null)
+			{
+				streamWriter.Dispose();
+				streamWriter = null;
+			}
+		}
+	}
+	
+	//THIS CODE BELOW LEAD TO UNHANDLED EXCEPTION
+	//DUE TO EARLY DISPOSED STREAM WRITER
+	
+	// public void StartWriting()
+	// {
+	// 	streamWriter.WriteLine("Hello!");
+	// 	streamWriter.Dispose();
+	// }
+	
+	// public void WriteAnother()
+	// {
+	// 	streamWriter.Write("Another Line");
+	// }
+}
