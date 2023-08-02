0b17f7e4c2e3c80a961ffd93b0b0128f534a9de9
MarcellinoSP <marcellinosp64@gmail.com>
2023-08-02 11:51:24 +0700

    Add Day 21

---
+++ Week 5/Day 21/Cancellation Token/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+
+class Program
+{
+	static async Task Main()
+	{
+		CancellationTokenSource source = new CancellationTokenSource();
+		CancellationToken token = source.Token;
+		
+		Task task = Task.Run(() => Constructing(token));
+		Task task1 = Task.Run(() => Cancelling(source));
+		
+		await task;
+		
+		Console.WriteLine("Constructing finished");
+	}
+	
+	static async Task Constructing(CancellationToken cancelToken)
+	{
+		//Versi 1, bakal kena delay ketika di cancel
+		// for(int i = 0; i <= 100; i++)
+		// {
+		// 	if(cancelToken.IsCancellationRequested)
+		// 	{
+		// 		return;
+		// 	}
+		// 	Console.WriteLine($"Constructing unit: {i}%");
+		// 	await Task.Delay(1000);
+		// }
+		
+		//Versi 2, sekali pencet langsung return
+		for(int i = 0; i <= 100; i++)
+		{
+			Console.WriteLine($"Constructing: {i}%");
+			try
+			{
+				await Task.Delay(5000, cancelToken);
+			}
+			catch(Exception message)
+			{
+				return;
+			}
+		}
+	}
+	
+	static async Task Cancelling(CancellationTokenSource sourceToken)
+	{
+		if(Console.ReadKey().KeyChar == ' ')
+		{
+			Console.WriteLine("token cancelled");
+			sourceToken.Cancel();
+		}
+	}
+}
---
+++ Week 5/Day 21/Race Condition/Auto Reset Event/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+
+class Program
+{
+    private static AutoResetEvent task1Event = new AutoResetEvent(false);
+    private static AutoResetEvent task3Event = new AutoResetEvent(false);
+
+    public static void Main()
+    {
+        // Create tasks for Task 1, Task 2, and Task 3
+        Task task1 = Task.Run(() => Task1Method());
+        Task task2 = Task.Run(() => Task2Method());
+        Task task3 = Task.Run(() => Task3Method());
+
+        // Wait for Task 1 and Task 3 to complete
+        Task.WaitAll(task1, task3);
+
+        Console.WriteLine("All tasks completed.");
+    }
+
+    private static void Task1Method()
+    {
+        Console.WriteLine("Task 1 is waiting for the signal...");
+        task1Event.WaitOne();
+        Console.WriteLine("Task 1 received the signal and started its work.");
+        // Task 1's work here...
+        Console.WriteLine("Task 1 completed its work.");
+    }
+
+    private static void Task2Method()
+    {
+        Console.WriteLine("Task 2 is doing some work...");
+        Thread.Sleep(2000);
+        Console.WriteLine("Task 2 is signaling Task 1 and Task 3 to start.");
+        task1Event.Set();
+        task3Event.Set();
+    }
+
+    private static void Task3Method()
+    {
+        Console.WriteLine("Task 3 is waiting for the signal...");
+        task3Event.WaitOne();
+        Console.WriteLine("Task 3 received the signal and started its work.");
+        Console.WriteLine("Task 3 completed its work.");
+    }
+}
