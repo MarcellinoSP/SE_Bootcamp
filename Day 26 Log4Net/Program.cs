389fe911a006d7e83aaa1e642856834454224eae
MarcellinoSP <marcellinosp64@gmail.com>
2023-08-11 11:42:18 +0700

    Add Logging

---
+++ Day 26 Log4Net/Program.cs
@@ -0,0 +1,16 @@
+﻿using log4net;
+using log4net.Config;
+
+namespace Log4NetTrial;
+
+internal class Program
+{
+	private static readonly ILog logger = LogManager.GetLogger(typeof(Program));
+	static void Main()
+	{
+		XmlConfigurator.Configure(new FileInfo ("log4net.config"));
+		
+		logger.Info("Program begin...");
+		logger.Debug("This message come as a debug");
+	}
+}
---
+++ Week 6/Day 26/Program.cs
@@ -0,0 +1,24 @@
+﻿using NLog;
+
+namespace NLogTrial
+{
+	class Program
+	{
+		private static Logger logger = LogManager.GetCurrentClassLogger();
+		static void Main()
+		{
+			var currentDirectory = Directory.GetCurrentDirectory();
+			var logConfigFile = Path.Combine(currentDirectory, "Nlog.config");
+			LogManager.LoadConfiguration(logConfigFile);
+			
+			logger.Debug("This is debug message");
+			logger.Trace("This is trace message");
+			logger.Info("This is info message");
+			logger.Warn("This is warning message");
+			logger.Error("This is error message");
+			logger.Fatal("This is fatal message");
+			
+			Console.ReadKey();
+		}
+	}
+}
